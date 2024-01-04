6ea685fdf65c717423472ae04f80fae1af398b26
Susovan <susovan@gmail.com>
2024-01-04 13:17:35 +0530

    banner added

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,20 @@
+<Fragment>
+{`import "./"
+
+
+`}<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="description" content="Astro description" />
+<meta name="viewport" content="width=device-width" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<meta name="generator" content={Astro.generator} />
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+</head>
+<body>
+
+</body>
+</html>
+</Fragment>;
